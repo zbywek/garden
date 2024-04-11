8454623bf86feabf20d2a3366b303aae6a54e775
zbywek <zbywek@gmail.com>
2024-04-11 20:31:29 +0200

    Cross global configuration, plus find by id endpoint

--- src/main/java/pl/infomind/garden/controller/DeviceController.java
+++ src/main/java/pl/infomind/garden/controller/DeviceController.java
@@ -1,14 +1,17 @@
 package pl.infomind.garden.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.infomind.garden.entity.Device;
+import pl.infomind.garden.model.DeviceResponse;
 import pl.infomind.garden.service.DeviceService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/devices")
 public class DeviceController {
@@ -16,12 +19,14 @@ public class DeviceController {
     private final DeviceService deviceService;
 
 
-    public DeviceController(DeviceService deviceService) {
-        this.deviceService = deviceService;
-    }
 
-    @GetMapping("/{deviceName}")
+    @GetMapping("/name/{deviceName}")
     public List<Device> findAllDevicesByDeviceName(@PathVariable String deviceName) {
         return deviceService.findAllDevicesByDeviceName(deviceName);
+    }
+
+    @GetMapping("/id/{id}")
+    public DeviceResponse findDeviceById(@PathVariable Long id) {
+        return deviceService.findDeviceById(id);
     }
 }
--- src/main/java/pl/infomind/garden/service/DeviceService.java
+++ src/main/java/pl/infomind/garden/service/DeviceService.java
@@ -1,22 +1,35 @@
 package pl.infomind.garden.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.infomind.garden.entity.Device;
+import pl.infomind.garden.map.DeviceMapper;
+import pl.infomind.garden.model.DeviceResponse;
 import pl.infomind.garden.repository.DeviceRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
+
+@RequiredArgsConstructor
 @Service
 public class DeviceService {
     private final DeviceRepository deviceRepository;
+    private final DeviceMapper deviceMapper;
 
-    @Autowired
-    public DeviceService(DeviceRepository deviceRepository) {
-        this.deviceRepository = deviceRepository;
-    }
 
     public List<Device> findAllDevicesByDeviceName(String deviceName) {
         return deviceRepository.findByDeviceName(deviceName);
+    }
+
+    public DeviceResponse findDeviceById(Long deviceId) {
+        Optional<Device> deviceOptional = deviceRepository.findById(deviceId);
+        if (deviceOptional.isPresent()) {
+            Device device = deviceOptional.get();
+            return deviceMapper.mapToResponse(device);
+        } else {
+            throw new NoSuchElementException("Device not found");
+        }
     }
 }
